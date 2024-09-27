f1cc1e5ca3bc907c1ba50891d3dc0c5e57b416c1
deyikong <kongdeyi0209@gmail.com>
2024-09-26 21:14:09 -0400

    Add files via upload

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Cleans the raw data of incidents into an analysis dataset
 # Author: Deyi Kong
-# Date: 23 September 2024
+# Date: 25 September 2024
 # Contact: deyi.kong@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: Need to have downloaded the data
@@ -10,36 +10,66 @@
 #### Workspace setup ####
 library(tidyverse)
 library(dplyr)
+library(lubridate)
 
 #### Clean data ####
 raw_data <- read_csv("data/raw_data/raw_data.csv")
 
-#colnames(raw_data)
+# get the column names of raw_data
+colnames(raw_data)
+
+#  check each variables' information for features selection
 summary(raw_data)
-typeof(raw_data$TFS_Alarm_Time)
-#unique(raw_data$Final_Incident_Type)
-na_indices_control <- raw_data %>%
-  filter(is.na(Fire_Under_Control_Time) & !is.na(Status_of_Fire_On_Arrival))
-summary(na_indices_control)
+
+# check how many different types are the final incidents
+unique(raw_data$Area_of_Origin)
+unique(raw_data$Building_Status)
+unique(raw_data$Business_Impact)
+unique(raw_data$Extent_Of_Fire)
+unique(raw_data$Final_Incident_Type)
+unique(raw_data$Fire_Alarm_System_Impact_on_Evacuation)
+unique(raw_data$Fire_Alarm_System_Operation)
+unique(raw_data$Fire_Alarm_System_Presence)
+levels(raw_data$Ignition_Source)
+
+# filter the data from 2018 to 2023 because that was the time period we care about
+current_data <- 
+  raw_data |>
+  mutate(
+    year = year(as.Date(TFS_Alarm_Time))
+  ) |>
+  filter((year >= 2018 & year <= 2023))
+
+# old_data <- 
+#   raw_data |>
+#   mutate(
+#     year = year(as.Date(TFS_Alarm_Time))
+#   ) |>
+#   filter(!(year >= 2018 & year <= 2023))
+# 
+# summary(old_data)
+# summary(current_data)
 
 cleaned_data <- 
-  raw_data |>
+  current_data |>
   janitor::clean_names() |>
-  select(area_of_origin, estimated_dollar_loss, final_incident_type, fire_alarm_system_impact_on_evacuation, fire_alarm_system_presence, ignition_source, smoke_alarm_impact_on_persons_evacuating_impact_on_evacuation, sprinkler_system_presence, status_of_fire_on_arrival, fire_under_control_time, tfs_alarm_time, tfs_arrival_time) |>
-
+  # selecting the variables that will be used later
+  select(year, area_of_origin, estimated_dollar_loss, fire_alarm_system_impact_on_evacuation, fire_alarm_system_presence, ignition_source, smoke_alarm_impact_on_persons_evacuating_impact_on_evacuation, sprinkler_system_presence, status_of_fire_on_arrival, fire_under_control_time, tfs_alarm_time, tfs_arrival_time) |>
   drop_na() |>
   filter(!is.na(status_of_fire_on_arrival) & !is.na(fire_under_control_time)) |>
-  # Calculate duration based on the parsed date-time
+  # Calculate duration and time spent for TFS to arrive based on the parsed date-time
   mutate(
     TFS_Notified_Till_Arrival = as.numeric(difftime(tfs_arrival_time, tfs_alarm_time, units = "secs")),
-    TFS_Duration = as.numeric(difftime(fire_under_control_time, tfs_arrival_time, units = "secs"))
+    TFS_Duration = as.numeric(difftime(fire_under_control_time, tfs_arrival_time, units = "secs")),
   )|>  
   # Parse the date-time strings directly
   mutate(
     tfs_alarm_time = parse_date_time(tfs_alarm_time, orders = c("ymd HMS", "ymd HMS"), quiet = TRUE),
     tfs_arrival_time = parse_date_time(tfs_arrival_time, orders = c("ymd HMS", "ymd HMS"), quiet = TRUE),
-    fire_under_control_time = parse_date_time(fire_under_control_time, "ymd HMS", quiet = TRUE)
-  ) |>
+    fire_under_control_time = parse_date_time(fire_under_control_time, orders = c("ymd HMS", "ymd HMS"), quiet = TRUE),
+    # If fire_under_control_time is NA, set it to "00:00:00"
+    fire_under_control_time = coalesce(fire_under_control_time, as.POSIXct("1970-01-01 00:00:00", tz = "UTC"))
+    ) |>
   # Extract YMD and format HMS
   mutate(
     estimated_dollar_loss = as.numeric(estimated_dollar_loss),
@@ -50,18 +80,20 @@ cleaned_data <-
     tfs_arrival_time = format(tfs_arrival_time, "%H:%M:%S"),
     fire_under_control_time = format(fire_under_control_time, "%H:%M:%S"),
   )
-summary(cleaned_data)
+#summary(cleaned_data)
 # Identify which entries were converted to NA
 
-na_indices <- cleaned_data %>%
-  filter(is.na(fire_under_control_time))
-print(na_indices)
+# na_indices <- cleaned_data %>%
+#   filter(is.na(fire_under_control_time))
+# 
+# na_indices_time <- cleaned_data %>%
+#   filter(is.na(TFS_Notified_Till_Arrival))
+#
+# na_indices_dur <- cleaned_data %>%
+#   filter(is.na(TFS_Duration))
+# print(na_indices_dur)
 
-na_indices_dur <- cleaned_data %>%
-  filter(is.na(TFS_Duration))
-print(na_indices_dur)
-
-?difftime
+#?difftime
 
 #### Save data ####
 write_csv(cleaned_data, "data/analysis_data/analysis_data.csv")
